16bf7bb28036588e00ea31cbe6b80e01c74ca4b1
Ray Chavez <raychavez@gogamestation.com>
2022-07-11 22:14:54 -0500

    cleaned up commented code

--- src/movie.jsx
+++ src/movie.jsx
@@ -4,8 +4,6 @@ const Movie = ({movie}) => {
   return (
     <div>
       <h4>{movie.title}</h4>
-      {/* <img src={movie.image} alt={movie.title}/> */}
-
       <img src={"https://image.tmdb.org/t/p/w500" + movie.poster_path} alt={movie.title}/>
       <p>{movie.overview}</p>
       <p><strong>Language:</strong> {movie.original_language}</p>
